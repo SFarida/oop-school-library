e34b907c42ebe2cf50c1c84d9a6a9db709cb44b5
SFarida <faridagueleu@gmail.com>
2023-04-28 17:07:30 +0100

    Add test for Teacher class

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,15 @@
+require './teacher'
+require './decorator'
+describe Teacher do
+  context 'Testing class Teacher' do
+
+    it 'should create a Teacher class' do
+      teacher = Teacher.new(34, 'Network', 'maximilianus')
+      expect(teacher.can_use_services).to eq(true)
+      capitalized_teacher = CapitalizeDecorator.new(teacher)
+      trimmed_teacher = TrimmerDecorator.new(capitalized_teacher)
+      expect(capitalized_teacher.correct_name).to eq('Maximilianus')
+      expect(trimmed_teacher.correct_name).to eq('Maximilian')
+    end
+  end
+end
