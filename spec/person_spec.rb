4bf0f1b14b543476ce863b35ba36bc89540fd964
SFarida <faridagueleu@gmail.com>
2023-04-28 16:29:02 +0100

    Add tests for Person class

---
+++ spec/person_spec.rb
@@ -0,0 +1,13 @@
+require './person'
+describe Person do
+  context 'Testing class Person' do
+
+    it 'should create a book' do
+      person = Person.new(12, 'Alex')
+      older_person = Person.new(19, 'Tom')
+      expect(person.age).to eq(12)
+      expect(person.of_age).to eq(false)
+      expect(older_person.of_age).to eq(true)
+    end
+  end
+end
