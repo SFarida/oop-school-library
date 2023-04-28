8e45627033dd5c4841cf9f03520abe38d2fb8a8d
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-04-28 09:09:01 -0600

    Test Book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,12 @@
+require './book' 
+
+describe Book do 
+   context "Testing class Book" do 
+      
+      it "should create a book" do 
+         bo = Book.new('title-1','author-1')          
+         expect(bo.title).to eq('title-1')
+      end 
+		
+   end 
+end
